1aabd0656c0bb6de90300297bb2d4b8524eee1ef
Edgar Ruiz <edgararuiz@gmail.com>
2021-05-03 14:50:03 -0500

    Adds help and deps

--- R/common.R
+++ R/common.R
@@ -1,17 +1,23 @@
+# @importFrom datasets mtcars
+# @importFrom stats lm predict
 
+#' Returns the mtcars data set
 #' @export
 show_data <- function() {
-  mtcars
+  datasets::mtcars
 }
 
+#' Fits a linear regression model over mtcars
 #' @export 
 show_model <- function() {
-  model <- lm(mpg ~ wt, data = mtcars)
+  model <- stats::lm(mpg ~ wt, data = datasets::mtcars)
   summary(model)$coefficients
 }
 
+#' Retuns a preduction based on a provided Weight
+#' @param x Value for the weight of the car
 #' @export 
 get_prediction <- function(x) {
-  model <- lm(mpg ~ wt, data = mtcars)
-  predict(model, newdata = data.frame(wt = x))
+  model <- stats::lm(mpg ~ wt, data = datasets::mtcars)
+  stats::predict(model, newdata = data.frame(wt = x))
 }
